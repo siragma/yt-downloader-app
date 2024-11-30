fa1d146645c18132a9be28b879c231a74472c10a
gocee <siragma@gmail.com>
2024-11-30 20:41:53 +0900

    debug off

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def serve_file(download_id, filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
